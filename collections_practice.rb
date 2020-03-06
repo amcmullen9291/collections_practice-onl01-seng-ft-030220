0407fbfdbbcc3f2fdfd8ffd1cd35077a78589885
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-06 19:36:41 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -35,12 +35,8 @@ end
 def add_s(array)
 i=0
 while i<array.length
- if 1!=2
-array.each{|word| word<<"s"}
-1+=1
-end i=0
-while i<array.length
- if 1!=2
-array.each{|word| word<<"s""}
-1+=1
-end end
+   if 1!=2
+  array.each{|word| word<<"s"}
+  end
+i+=1
+end 
